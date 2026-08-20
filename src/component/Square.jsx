ddd71db22bb717d1686c64ed3475f518b5063ca2
Harena Livarijaona <liva13octobre2010@gmail.com>
2026-08-20 14:23:22 +0300

    x en rouge

--- src/component/Square.jsx
+++ src/component/Square.jsx
@@ -2,7 +2,8 @@
 function Square({ value, onSquareClick, isWinning }) {
   return (
     <button
-      className={`square ${isWinning ? 'winning' : ''}`}
+      className={`square ${isWinning ? 'winning' : ''} ${value === 'X' ? 'x' : ''
+        }`}
       onClick={onSquareClick}
     >
       {value}
